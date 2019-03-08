3b5c731cac5b44a94e8ab48f952aca05765264e1
EliasKioni <eliaskioni94@gmail.com>
2019-03-08 10:11:06 +0300

    assert deck of cards has 52 cards

---
+++ Tests/DeckOfCards.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from deck_of_cards import DeckOfCards
+
+
+class TestDeckOfCards(TestCase):
+
+    def test_deck_of_cards_has_52_cards(self):
+        deck_of_cards: DeckOfCards = DeckOfCards()
+        self.assertTrue(len(deck_of_cards) == 52)

180549251087e4cfc96e15abf06652d81421b56f
EliasKioni <eliaskioni94@gmail.com>
2019-03-08 11:00:36 +0300

    add method to validate Card rank

--- card.py
+++ card.py
@@ -1,3 +1,13 @@
-class Card:
-    rank = 1
+class Card(object):
+
+    def __init__(self, rank):
+        self.validate_rank(rank)
+        self.rank = rank
+
+    @staticmethod
+    def validate_rank(rank):
+        if 2 <= rank <= 14:
+            return True
+        raise Exception
+
     suit = 'Diamonds'

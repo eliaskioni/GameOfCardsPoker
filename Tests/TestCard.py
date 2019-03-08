5ad5c84aaebbfa1300d6b9799d261614bc1392f6
EliasKioni <eliaskioni94@gmail.com>
2019-03-08 11:00:16 +0300

    add test to validate card ranks

--- Tests/TestCard.py
+++ Tests/TestCard.py
@@ -23,13 +23,56 @@ class Ranks(Enum):
 class TestCard(TestCase):
 
     def test_card_has_rank_attribute(self):
-        card: Card = Card()
+        card: Card = Card(rank=Ranks.A.value)
         self.assertTrue(hasattr(card, 'rank'))
 
     def test_card_has_suit_attribute(self):
-        card: Card = Card()
+        card: Card = Card(rank=Ranks.A.value)
         self.assertTrue(hasattr(card, 'suit'))
 
-    def test_card_rank_attribute_only_holds_valid_values(self):
-        card: Card = Card(rank=14)
+    def test_card_rank_attribute_holds_valid_values(self):
+        card: Card = Card(rank=Ranks.A.value)
         self.assertTrue(card.rank == 14)
+
+        card: Card = Card(rank=Ranks.K.value)
+        self.assertTrue(card.rank == 13)
+
+        card: Card = Card(rank=Ranks.Q.value)
+        self.assertTrue(card.rank == 12)
+
+        card: Card = Card(rank=Ranks.J.value)
+        self.assertTrue(card.rank == 11)
+
+        card: Card = Card(rank=Ranks.TEN.value)
+        self.assertTrue(card.rank == 10)
+
+        card: Card = Card(rank=Ranks.NINE.value)
+        self.assertTrue(card.rank == 9)
+
+        card: Card = Card(rank=Ranks.EIGHT.value)
+        self.assertTrue(card.rank == 8)
+
+        card: Card = Card(rank=Ranks.SEVEN.value)
+        self.assertTrue(card.rank == 7)
+
+        card: Card = Card(rank=Ranks.SIX.value)
+        self.assertTrue(card.rank == 6)
+
+        card: Card = Card(rank=Ranks.FIVE.value)
+        self.assertTrue(card.rank == 5)
+
+        card: Card = Card(rank=Ranks.FOUR.value)
+        self.assertTrue(card.rank == 4)
+
+        card: Card = Card(rank=Ranks.THREE.value)
+        self.assertTrue(card.rank == 3)
+
+        card: Card = Card(rank=Ranks.TWO.value)
+        self.assertTrue(card.rank == 2)
+
+    def test_card_rank_attribute_throws_exception_on_invalid_values(self):
+        with self.assertRaises(Exception):
+            Card(rank=15)
+            Card(rank=None)
+            Card(rank=1)
+            Card()

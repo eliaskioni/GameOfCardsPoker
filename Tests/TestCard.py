48b7a28cc73794d9cf94f1ca0fa10169c480dedc
EliasKioni <eliaskioni94@gmail.com>
2019-03-08 10:05:56 +0300

    correct linting issues

--- Tests/TestCard.py
+++ Tests/TestCard.py
@@ -6,9 +6,9 @@ from card import Card
 class TestCard(TestCase):
 
     def test_card_has_rank_attribute(self):
-        card : Card = Card()
+        card: Card = Card()
         self.assertTrue(hasattr(card, 'rank'))
 
     def test_card_has_suit_attribute(self):
-        card : Card = Card()
+        card: Card = Card()
         self.assertTrue(hasattr(card, 'suit'))
